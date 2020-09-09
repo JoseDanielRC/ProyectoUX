703e295f4fe39c39df64fbea1a948d3e79f66d55
Jasser Ramos <ramosjasser@gmail.com>
2020-09-09 12:41:27 -0600

    kil

--- e-wallet/src/index.js
+++ e-wallet/src/index.js
@@ -5,18 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from "jquery";
-import * as firebase from 'firebase';
-const firebaseConfig = {
-  apiKey: "AIzaSyBxPpR8XFIMkMyGGyWS7vDWHXx222pzDw4",
-  authDomain: "e-wallet-51d51.firebaseapp.com",
-  databaseURL: "https://e-wallet-51d51.firebaseio.com",
-  projectId: "e-wallet-51d51",
-  storageBucket: "e-wallet-51d51.appspot.com",
-  messagingSenderId: "1074583859206",
-  appId: "1:1074583859206:web:fbc4c8b84aafb0ccfb1387",
-  measurementId: "G-VG7SCQYRTZ"
-};
-firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
