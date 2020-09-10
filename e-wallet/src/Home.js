f548f550c141444c6a64819c98d900eabae221e9
Jasser Ramos <ramosjasser@gmail.com>
2020-09-10 01:08:29 -0600

    agregar tarjetas

--- e-wallet/src/Home.js
+++ e-wallet/src/Home.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import fire from './Fire';
+import Tarjeta from './Tarjeta';
+function SubmissionBanner(props) {
+    if (!props.submit) {
+        return null;
+    }
+
+    return (
+        <div >
+            <Tarjeta/>
+        </div>
+    );
+}
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showComponent: false,
+            shouldShowButton: false
+        };
         this.logout = this.logout.bind(this);
+        this._onButtonClick = this._onButtonClick.bind(this);
     }
-
-
+    _onButtonClick() {
+        this.setState(prevState => ({
+            showComponent: true,
+            shouldShowButton : !prevState.shouldShowButton
+        }));
+    }
     logout() {
         fire.auth().signOut();
     }
-
     render() {
+
         return (
-            <div class="d-flex justify-content-center align-items-center login-container">
-                <form class="login-form text-center">
-                    <h1 class="mb-5 font-weight-light text-uppercase">Ha iniciado sesion</h1>
+
+            <div >
+                <form >
+                    <h1 class="mb-5 font-weight-light text-uppercase text-center">Ha iniciado sesion</h1>
+                    <SubmissionBanner submit={this.state.shouldShowButton} />
+                    <button type="button" class="btn mt-5 rounded-pill btn-lg btn-custom btn-block text-uppercase" onClick={this._onButtonClick} >
+                    {this.state.shouldShowButton ? "Volver" : "Agregar Tarjeta"}
+                 </button>
                     <button type="submit" class="btn mt-5 rounded-pill btn-lg btn-custom btn-block text-uppercase" onClick={this.logout} >Logout
                  </button>
                 </form>
