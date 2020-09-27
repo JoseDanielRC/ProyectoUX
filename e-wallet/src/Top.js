1ebffbff0557fac1a0731189c07243a162893e14
Jasser Ramos <ramosjasser@gmail.com>
2020-09-26 22:35:01 -0600

    Chat integrado

---
+++ e-wallet/src/Top.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Amigos.css';
+
+function Top() {
+    return (
+        <>
+            <div className="brand">
+                Enviar mensajes a sus amigos
+			</div>
+        </>
+    )
+}
+
+export default Top;
